4822e227938f09e7223ac6014e1d760de949016b
sbgrb <974487599@qq.com>
2021-03-10 23:46:30 +0800

    添加loading

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
-    <g-icon class="loading" name="Loading"></g-icon>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+    <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+    <g-icon class="loading icon" v-if="loading" name="Loading"></g-icon>
     <div class="icon-content">
       <slot></slot>
     </div>
@@ -12,6 +12,9 @@ export default {
   // props: ['icon', 'iconPosition']
   props:{
     icon:{},
+    loading:{
+      type:Boolean,
+    },
     iconPosition:{
       type:String,
       default:'left',
